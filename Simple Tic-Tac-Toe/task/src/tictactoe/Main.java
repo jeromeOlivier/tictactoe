ab7c6120e99aa3e4b615f1ff98e9ae4c7a3f24ce
jeromeolivier <jerome.olivier@icloud.com>
2021-07-10 14:38:36 -0400

    First version completed.

--- Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -4,109 +4,115 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Printer printer = new Printer();
-        GameLogic logic = new GameLogic();
+        Printer print = new Printer();
+        Game game = new Game();
         
-        printer.drawBoard(logic.getGame());
-        
-        logic.numericalInput();
+        print.board(game.fetch());
+        game.play();
     }
 }
 
-class GameLogic {
+class Game {
+    Scanner scanner = new Scanner(System.in);
+    Printer print = new Printer();
     private int[][] game = new int[3][3];
+    int player = 1;
     
-    public int[][] getGame() {
+    public int[][] fetch() {
         return game;
     }
     
-    public void setGame(int[][] game) {
-        this.game = game;
-    }
-    
-    public void numericalInput() {
-        Scanner scanner = new Scanner(System.in);
-        Printer printer = new Printer();
-        
-        System.out.print("Enter the coordinates: ");
+    public void play() {
+        print.requestCoordinates();
         String[] c = scanner.nextLine().split("\\s");
         int row = 0, col = 0;
         try {
             row = Integer.parseInt(c[0]);
             col = Integer.parseInt(c[1]);
         } catch (NumberFormatException e) {
-            System.out.println("You should enter numbers!");
-            numericalInput();
+            print.requestNumbersOnly();
+            play();
         }
         if (row > 3 || col > 3 || row < 1 || col < 1) {
-            System.out.println("Coordinates should be from 1 to 3!");
-            numericalInput();
+            print.requestStayWithinRange();
+            play();
         } else if (Math.abs(game[row - 1][col - 1]) == 1) {
-            System.out.println("This cell is occupied! Choose another one!");
-            numericalInput();
+            print.requestUnusedCoordinates();
+            play();
         } else {
-            game[row - 1][col - 1] = 1;
-            printer.drawBoard(game);
-        }
-        
-    }
-    
-    public int[] analyzeBoard(int[][] game) {
-        int blank = 9, xScore = 0, yScore = 0, xCount = 0, yCount = 0, xDiagonal = 0, yDiagonal = 0;
-        for (int i = 0; i < game.length; i++) {
-            int rowTotal = 0, columnTotal = 0, diag01Total = 0, diag02Total = 0;
-            for (int j = 0; j < game[i].length; j++) {
-                if (game[i][j] != 0) {
-                    --blank;
+            game[row - 1][col - 1] = player;
+            print.board(game);
+            int numberOfPlays = 0;
+            for (int i = 0; i < game.length; i++) {
+                int rowTotal = 0, columnTotal = 0, diag01Total = 0, diag02Total = 0;
+                for (int j = 0; j < game[i].length; j++) {
+                    rowTotal = rowTotal + game[i][j];
+                    columnTotal = columnTotal + game[j][i];
+                    diag01Total = diag01Total + game[j][j];
+                    diag02Total = diag02Total + game[2 - j][j];
+                    if (game[i][j] != 0) {++numberOfPlays;}
                 }
-                rowTotal = rowTotal + game[i][j];
-                columnTotal = columnTotal + game[j][i];
-                diag01Total = diag01Total + game[j][j];
-                diag02Total = diag02Total + game[2 - j][j];
-                if (rowTotal == 3
-                        || columnTotal == 3
-                        || diag01Total == 3
-                        || diag02Total == 3) {
-                    xScore++;
-                    xDiagonal++;
-                }
-                if (rowTotal == -3
-                        || columnTotal == -3
-                        || diag01Total == -3
-                        || diag02Total == -3) {
-                    yScore++;
-                    yDiagonal++;
-                }
-                if (game[i][j] == 1) {
-                    ++xCount;
-                } else if (game[i][j] == -1) {
-                    ++yCount;
+                if (Math.abs(rowTotal) == 3
+                        || Math.abs(columnTotal) == 3
+                        || Math.abs(diag01Total) == 3
+                        || Math.abs(diag02Total) == 3) {
+                    print.declareWinner(player);
+                    return;
                 }
             }
+            if (numberOfPlays == 9) {
+                print.declareDraw();
+                return;
+            }
+            player = player == 1 ? -1 : 1;
+            play();
         }
-        return new int[]{blank, xScore, yScore, xCount, yCount, xDiagonal, yDiagonal};
     }
 }
 
 class Printer {
     
-    public void drawBoard(int[][] game) {
+    public void board(int[][] game) {
         System.out.println("---------");
         int increment = 0;
         int value;
         char mark;
-        for (int i = 0; i < 3; i++) {
+        for (int i = 1; i < 4; i++) {
             for (int j = 0; j < 5; j++) {
                 if (j == 0 || j == 4) {
                     System.out.print("| ");
                 } else {
-                    value = game[i][j - 1];
-                    mark = value == -1 ? 'O' : value == 1 ? 'X' : '_';
+                    value = game[i - 1][j - 1];
+                    mark = value == -1 ? 'O' : value == 1 ? 'X' : ' ';
                     System.out.print(mark + " ");
                 }
             }
             System.out.print("\n");
         }
         System.out.println("---------");
+    }
+    
+    public void requestCoordinates() {
+        System.out.print("Enter the coordinates: ");
+    }
+    
+    public void requestNumbersOnly() {
+        System.out.println("You should enter numbers!");
+    }
+    
+    public void requestStayWithinRange() {
+        System.out.println("Coordinates should be from 1 to 3!");
+    }
+    
+    public void requestUnusedCoordinates() {
+        System.out.println("This cell is occupied! Choose another one!");
+    }
+    
+    public void declareWinner(int player) {
+        System.out.println(player == 1 ? "X wins" : "O wins");
+    }
+    
+    public void declareDraw() {
+        System.out.println("Draw");
     }
 }
